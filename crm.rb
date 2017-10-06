95af09bd4a7a9955e1b56131616e00e1319d1b07
Michelle Cinello <m.cinello@gmail.com>
2017-10-06 15:40:21 -0400

    added a 404 page

--- crm.rb
+++ crm.rb
@@ -12,7 +12,10 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else raise Sinatra::NotFound
+  end
 end
 
 get '/about' do
