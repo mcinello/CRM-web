66b2620145eacc2f76bdfbb49ef86aa3ed9dd194
Michelle Cinello <m.cinello@gmail.com>
2017-10-06 21:12:23 -0400

    forgot a / in /contacts/:id/edit; the request now works.

--- crm.rb
+++ crm.rb
@@ -33,14 +33,30 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-get 'contacts/:id/edit' do
-  @contact = Contact.find_by(id: params[:id].to_i)
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
     erb :edit_contact
   else
     raise Sinatra::NotFound
   end
 end
+
+# post 'contacts/:id' do
+#   @contact = Contact.find_by(id: params[:id].to_i)
+#   if @contact
+#     @contact.update(
+#     first_name: params[:first_name],
+#     last_name: params[:last_name],
+#     email: params[:email],
+#     note: params[:note]
+#     )
+#
+#     redirect to('/contacts')
+#   else
+#     raise Sinatra::NotFound
+#   end
+# end
 
 get '/about' do
   erb :about
